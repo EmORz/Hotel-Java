f7a3e62cd7b527628eba3f8f03fbe888a3c89cfa
Emanuil <eiv.razgrad@gmail.com>
2023-06-21 21:21:43 +0300

    Implement method getStats and formatted cod.

--- src/Main.java
+++ src/Main.java
@@ -14,23 +14,29 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         byte choice;
 
-        while (true){
+        while (true) {
             System.out.println("Please select what you want to do:");
             System.out.println("1 - Make a reservation.");
             System.out.println("2 - List free rooms.");
             System.out.println("3 - Checkout room.");
+            System.out.println("4 - Stats.");
             System.out.println("5 - Find room.");
             System.out.println("6 - Update a room.");
             System.out.println("0 - Exit");
             choice = scanner.nextByte();
-            switch (choice){
+            switch (choice) {
                 case 1:
                     makeReservation(scanner);
                     break;
                 case 2:
                     listFreeRooms(scanner);
+                    break;
                 case 3:
                     checkoutRoom(scanner);
+                    break;
+                case 4:
+                    getStats(scanner);
+                    break;
                 case 5:
                     findRoom(scanner);
                     break;
@@ -45,6 +51,28 @@ public class Main {
 
     }
 
+    private static void getStats(Scanner scanner) {
+        System.out.print("Enter start date: ");
+        LocalDate startDate = LocalDate.parse(scanner.next(), DATE_FORMATTER);
+        System.out.print("Enter end date: ");
+        LocalDate endDate = LocalDate.parse(scanner.next(), DATE_FORMATTER);
+
+        System.out.println("Room usage stats: ");
+        for (Map.Entry<Integer, Reservation> entry : reservations.entrySet()) {
+
+            int roomNumber = entry.getKey();
+            Reservation reservation = entry.getValue();
+            LocalDate startDateReservation = reservation.getStartDate();
+            LocalDate endDateReservation = reservation.getEndDate();
+            LocalDate start = startDate.isAfter(startDateReservation) ? startDate : startDateReservation;
+            LocalDate end = endDateReservation.isBefore(endDate) ? endDate : endDateReservation;
+            long daysOfUsage = end.toEpochDay() - start.toEpochDay();
+            System.out.println(roomNumber + ": " + daysOfUsage + " days.");
+
+
+        }
+    }
+
     private static void findRoom(Scanner scanner) {
 
         System.out.print("Enter number of beds: ");
@@ -56,22 +84,22 @@ public class Main {
 
         List<Integer> avaibleRooms = new ArrayList<>();
 
-        for (Map.Entry<Integer, Room> entry: rooms.entrySet()
-             ) {
+        for (Map.Entry<Integer, Room> entry : rooms.entrySet()
+        ) {
             int roomNumber = entry.getKey();
             Room room = entry.getValue();
 
-            if (!reservations.containsKey(roomNumber) && room.getNumberOfBeds()>=beds) {
+            if (!reservations.containsKey(roomNumber) && room.getNumberOfBeds() >= beds) {
                 avaibleRooms.add(roomNumber);
             }
         }
 
         if (avaibleRooms.isEmpty()) {
             System.out.print("No avaiable rooms matching criteria.");
-        }else {
+        } else {
             System.out.print("Available rooms: ");
-            for (Integer roomNum: avaibleRooms
-                 ) {
+            for (Integer roomNum : avaibleRooms
+            ) {
                 System.out.println(roomNum);
             }
         }
@@ -80,12 +108,12 @@ public class Main {
     private static void listFreeRooms(Scanner scanner) {
         List<Integer> freeRooms = new ArrayList<>();
 
-        for (Map.Entry<Integer,Room> entry: rooms.entrySet()) {
+        for (Map.Entry<Integer, Room> entry : rooms.entrySet()) {
             int roomNumber = entry.getKey();
             Room room = entry.getValue();
 
             if (!reservations.containsKey(roomNumber)) {
-               freeRooms.add(roomNumber);
+                freeRooms.add(roomNumber);
             }
         }
 
@@ -93,8 +121,8 @@ public class Main {
             System.out.println("No free rooms.");
         } else {
             System.out.println("Available rooms: ");
-            for (Integer roomNumber: freeRooms
-                 ) {
+            for (Integer roomNumber : freeRooms
+            ) {
                 System.out.println(roomNumber);
 
             }
@@ -111,8 +139,7 @@ public class Main {
         }
 
         Reservation reservation = reservations.remove(roomNumber);
-        System.out.println("Room "+roomNumber+" has been check out.");
-
+        System.out.println("Room " + roomNumber + " has been check out.");
 
     }
 
@@ -131,7 +158,7 @@ public class Main {
         String notes = scanner.nextLine();
         room.setNotes(notes);
 
-        System.out.println("Room "+roomNumber+" has been update.");
+        System.out.println("Room " + roomNumber + " has been update.");
     }
 
     private static void makeReservation(Scanner scanner) {
@@ -154,7 +181,7 @@ public class Main {
 
         Reservation reservation = new Reservation(roomNumber, startDate, endDate, notes);
         reservations.put(roomNumber, reservation);
-        System.out.print("Reservation was created!");
+        System.out.println("Reservation was created!");
 
     }
 }
